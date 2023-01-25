f0b88abb732c097fe059ce67f1e42893c8bf0b46
Ludovic Lahougue <ludo.lahougue@orange.fr>
2023-01-25 12:19:22 +0100

    mobile v1

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -2,3 +2,9 @@ function copy() {
     navigator.clipboard.writeText('contacter.dana@gmail.com');
     alert("L'adresse mail a été copiée dans le presse papier.");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('toggleMenu').addEventListener('click', () => {
+        document.getElementById('topNav').classList.toggle('responsive')
+    })
+})
