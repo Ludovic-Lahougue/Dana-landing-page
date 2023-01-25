3e4398f403824930fcfeebfdbb39e9bce397e573
Ludovic Lahougue <ludo.lahougue@orange.fr>
2023-01-25 15:05:44 +0100

    mobile fix

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -4,7 +4,30 @@ function copy() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('toggleMenu').addEventListener('click', () => {
-        document.getElementById('topNav').classList.toggle('responsive')
+    document.addEventListener('click', event => {
+        const menu = document.getElementById('topNav')
+        const links = menu.getElementsByTagName('a')
+        for(link of links) {
+            link.addEventListener('click', () => {
+                document.getElementById('topNav').classList.remove('active')
+                checkScroll()
+            })
+        }
+        const menuIcon = document.getElementById('toggleMenu')
+        if(!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+            document.getElementById('topNav').classList.remove('active')
+            checkScroll()
+        }
+    })
+
+    document.getElementById('toggleMenu').addEventListener('click', event => {
+        document.getElementById('topNav').classList.toggle('active')
     })
 })
+
+function checkScroll() {
+    let html = document.querySelector('html')
+    if(document.getElementById('topNav').classList.contains('active'))
+        html.style.overflow = 'hidden'
+    else html.style.overflow = 'scroll'
+}
